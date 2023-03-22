72c9ff3c0114762d4e0508d1cf1381b220eb0335
Flavio Lima <flaviolima.s@live.com>
2023-03-22 18:49:41 -0300

    WIP - mapeamento de objeto responsavel pela transformacao de objetos

--- src/main/java/com/sicred/poc/mapper/AgendaMapper.java
+++ src/main/java/com/sicred/poc/mapper/AgendaMapper.java
@@ -1,8 +1,24 @@
 package com.sicred.poc.mapper;
 
+import com.sicred.poc.external.dto.AgendaDTO;
+import com.sicred.poc.model.AgendaEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class AgendaMapper {
 
+    public List<AgendaDTO> from(List<AgendaEntity> agendas) {
+        List<AgendaDTO> agendasDTO = new ArrayList<>();
+        agendas.forEach(agenda ->
+            agendasDTO.add(AgendaDTO.builder()
+                .id(agenda.getId())
+                .title(agenda.getTitle())
+                .description(agenda.getDescription())
+            .build())
+        );
+        return agendasDTO;
+    }
 }
