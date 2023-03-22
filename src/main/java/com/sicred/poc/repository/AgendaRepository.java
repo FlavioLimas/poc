a76b3aff93c04e4cfb1c848109f2483095617b45
Flavio Lima <flaviolima.s@live.com>
2023-03-22 17:27:00 -0300

    WIP - add esteriotipo

--- src/main/java/com/sicred/poc/repository/AgendaRepository.java
+++ src/main/java/com/sicred/poc/repository/AgendaRepository.java
@@ -2,6 +2,8 @@ package com.sicred.poc.repository;
 
 import com.sicred.poc.model.AgendaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AgendaRepository extends JpaRepository<Long, AgendaEntity> {
 }
