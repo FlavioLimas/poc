4deab0c1b786e63e7036d603633745c2c6ebc7e1
Flavio Lima <flaviolima.s@live.com>
2023-03-22 17:39:29 -0300

    WIP - definicao de Exception para o micro service

---
+++ src/main/java/com/sicred/poc/exception/PocSicredException.java
@@ -0,0 +1,16 @@
+package com.sicred.poc.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class PocSicredException extends Exception {
+
+    public PocSicredException(String message, Exception exception) {
+        super(message, exception);
+    }
+
+    public PocSicredException(PocSicredErrors message) {
+        super(message.getMessage());
+    }
+}
