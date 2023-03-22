af2eed5b5017ca86ff29915af81fc3ab0535919e
Flavio Lima <flaviolima.s@live.com>
2023-03-22 18:45:30 -0300

    WIP - definicao de interface para servico

--- src/main/java/com/sicred/poc/service/IAgendaService.java
+++ src/main/java/com/sicred/poc/service/IAgendaService.java
@@ -1,20 +1,19 @@
 package com.sicred.poc.service;
 
 import com.sicred.poc.external.dto.AgendaDTO;
-import com.sicred.poc.model.AgendaEntity;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IAgendaService {
 
-    Optional<List<AgendaEntity>> getAll();
+    List<AgendaDTO> getAll();
 
-    Optional<AgendaEntity> findByName(String title);
+    Optional<AgendaDTO> findByName(String title);
 
-    Optional<AgendaEntity> save(AgendaDTO agendaDTO);
+    Optional<AgendaDTO> save(AgendaDTO agendaDTO);
 
-    Optional<AgendaEntity> update(AgendaDTO agendaDTO);
+    Optional<AgendaDTO> update(AgendaDTO agendaDTO);
 
     void delete(AgendaDTO agendaDTO);
 
