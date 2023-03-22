58c9f09f128c3669073a07e792a856d73d2845bd
Flavio Lima <flaviolima.s@live.com>
2023-03-22 17:24:33 -0300

    WIP - renomeado classe

--- src/main/java/com/sicred/poc/model/AgendaEntity.java
+++ src/main/java/com/sicred/poc/model/AgendaEntity.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @EqualsAndHashCode
-public class Agenda {
+public class AgendaEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
