5d23a7f93d193e5480ebbc0a0540722bf1cd10b0
Flavio Lima <flaviolima.s@live.com>
2023-03-23 13:05:17 -0300

    WIP - renomeando exception

--- src/main/java/com/assembleia/poc/exception/PocAssembleiaException.java
+++ src/main/java/com/assembleia/poc/exception/PocAssembleiaException.java
@@ -4,13 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-public class PocSicredException extends Exception {
+public class PocAssembleiaException extends Exception {
 
-    public PocSicredException(String message, Exception exception) {
+    public PocAssembleiaException(String message, Exception exception) {
         super(message, exception);
     }
 
-    public PocSicredException(PocSicredErrors message) {
+    public PocAssembleiaException(PocSicredErrors message) {
         super(message.getMessage());
     }
 }
