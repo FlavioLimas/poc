21143b3693280952ae0fb2266e47d5cf094beddd
Flavio Lima <flaviolima.s@live.com>
2023-03-23 13:15:16 -0300

    WIP - implementado servico para findByTitle

--- src/main/java/com/assembleia/poc/service/impl/AgendaService.java
+++ src/main/java/com/assembleia/poc/service/impl/AgendaService.java
@@ -1,12 +1,12 @@
 package com.assembleia.poc.service.impl;
 
+import com.assembleia.poc.exception.PocAssembleiaException;
 import com.assembleia.poc.exception.PocSicredErrors;
 import com.assembleia.poc.external.dto.AgendaDTO;
-import com.assembleia.poc.service.IAgendaService;
-import com.assembleia.poc.exception.PocSicredException;
 import com.assembleia.poc.mapper.AgendaMapper;
 import com.assembleia.poc.model.AgendaEntity;
 import com.assembleia.poc.repository.AgendaRepository;
+import com.assembleia.poc.service.IAgendaService;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -28,14 +28,21 @@ public class AgendaService implements IAgendaService {
         List<AgendaEntity> agendas = repository.findAll();
         if (agendas.isEmpty()) {
             log.error("Agenda " + agendas);
-            throw new PocSicredException(PocSicredErrors.AGENDA_NOT_FOUND);
+            throw new PocAssembleiaException(PocSicredErrors.AGENDA_NOT_FOUND);
         }
         return mapper.from(agendas);
     }
 
     @Override
-    public AgendaDTO findByName(String title) {
-        return null;
+    @SneakyThrows
+    public AgendaDTO findByTitle(String title) {
+        if (title == null || title.trim().isEmpty()) {
+            log.error("Title " + title);
+            throw new PocAssembleiaException(PocSicredErrors.VALUE_INVALID);
+        }
+        AgendaEntity agenda = repository.findByTitle(title).orElseThrow(() ->
+                new PocAssembleiaException(PocSicredErrors.AGENDA_NOT_FOUND));
+        return mapper.from(agenda);
     }
 
     @Override
