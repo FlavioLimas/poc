760f99d07dea04f49a173750f24a96609484f99f
Flavio Lima <flaviolima.s@live.com>
2023-03-23 17:12:09 -0300

    WIP - implementado transformacao de objeto para save

--- src/main/java/com/sicred/poc/mapper/AgendaMapper.java
+++ src/main/java/com/sicred/poc/mapper/AgendaMapper.java
@@ -1,7 +1,10 @@
 package com.sicred.poc.mapper;
 
+import com.sicred.poc.exception.PocAssembleiaException;
+import com.sicred.poc.exception.PocSicredErrors;
 import com.sicred.poc.external.dto.AgendaDTO;
 import com.sicred.poc.model.AgendaEntity;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -13,11 +16,11 @@ public class AgendaMapper {
     public List<AgendaDTO> from(List<AgendaEntity> agendas) {
         List<AgendaDTO> agendasDTO = new ArrayList<>();
         agendas.forEach(agenda ->
-            agendasDTO.add(AgendaDTO.builder()
-                .id(agenda.getId())
-                .title(agenda.getTitle())
-                .description(agenda.getDescription())
-            .build())
+                agendasDTO.add(AgendaDTO.builder()
+                        .id(agenda.getId())
+                        .title(agenda.getTitle())
+                        .description(agenda.getDescription())
+                        .build())
         );
         return agendasDTO;
     }
@@ -27,6 +30,17 @@ public class AgendaMapper {
                 .id(agenda.getId())
                 .title(agenda.getTitle())
                 .description(agenda.getDescription())
+                .build();
+    }
+
+    @SneakyThrows
+    public AgendaEntity toSave(AgendaDTO agendaDTO) {
+        if (null != agendaDTO.getId())
+            throw new PocAssembleiaException(PocSicredErrors.ID_MUST_BE_NULL_WHEN_SAVE);
+        return AgendaEntity.builder()
+                .id(agendaDTO.getId())
+                .title(agendaDTO.getTitle())
+                .description(agendaDTO.getDescription())
                 .build();
     }
 }
