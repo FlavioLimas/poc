b1b77e70b96142b2bc7ebaa1ba37288be9355d1a
Flavio Lima <flaviolima.s@live.com>
2023-03-23 22:25:10 -0300

    WIP - mapeamento do objeto da votacao

--- src/main/java/com/sicred/poc/mapper/VotingMapper.java
+++ src/main/java/com/sicred/poc/mapper/VotingMapper.java
@@ -1,4 +1,14 @@
 package com.sicred.poc.mapper;
 
+import com.sicred.poc.external.dto.AgendaDTO;
+import com.sicred.poc.external.dto.AssociateDTO;
+import com.sicred.poc.external.dto.VotingSavedDTO;
+import com.sicred.poc.model.VotingEntity;
+
 public class VotingMapper {
+
+    public VotingEntity to(AssociateDTO associateDTO, AgendaDTO agendaDTO, VotingSavedDTO votingSavedDTO) {
+        return null;
+    }
+
 }
