80b94d2cd6c3f4242dcf0338df88c042c588fbd8
Flavio Lima <flaviolima.s@live.com>
2023-03-24 17:58:26 -0300

    WIP - Add test para getAll e findByTitle

---
+++ src/test/java/com/sicred/poc/controller/AgendaControllerTest.java
@@ -0,0 +1,66 @@
+package com.sicred.poc.controller;
+
+import com.sicred.poc.external.dto.AgendaDTO;
+import com.sicred.poc.service.impl.AgendaService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AgendaControllerTest {
+
+    @InjectMocks
+    private AgendaController controller;
+    @Mock
+    private AgendaService service;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Should Pass When GetAll OK")
+    void testShouldPassWhenGetAllOK() {
+        ResponseEntity<List<AgendaDTO>> response = controller.getAll();
+        assertEquals(HttpStatus.OK, response.getStatusCode(),
+                "Assertion fail, response status invalid");
+    }
+
+    @Test
+    @DisplayName("Should Pass When FindByTitle OK")
+    void testShouldPassWhenFindByTitleOK() {
+        ResponseEntity<AgendaDTO> response = controller.findByTitle("Titulo da pauta teste");
+        assertEquals(HttpStatus.OK, response.getStatusCode(),
+                "Assertion fail, response status invalid");
+    }
+
+    @Test
+    @DisplayName("Should Pass When Save CREATED")
+    void testShouldPassWhenSaveCREATED() {
+
+        ResponseEntity<AgendaDTO> response = controller.save();
+        assertEquals(HttpStatus.CREATED, response.getStatusCode(),
+                "Assertion fail, response status invalid");
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+}
