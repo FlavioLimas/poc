36aa3a5990657651fc737e02b3ef91d4897161c6
Flavio Lima <flaviolima.s@live.com>
2023-03-24 17:36:28 -0300

    WIP - ajuste regra

--- src/main/java/com/sicred/poc/mapper/VotingMapper.java
+++ src/main/java/com/sicred/poc/mapper/VotingMapper.java
@@ -7,7 +7,7 @@ import com.sicred.poc.external.dto.AssociateDTO;
 import com.sicred.poc.external.dto.VotingDTO;
 import com.sicred.poc.external.dto.VotingSavedDTO;
 import com.sicred.poc.model.AgendaEntity;
-import com.sicred.poc.model.VoteEntity;
+import com.sicred.poc.model.VotingEntity;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
@@ -21,8 +21,11 @@ public class VotingMapper {
     private AssociateMapper associateMapper;
     private AgendaMapper agendaMapper;
 
-    public VoteEntity to(AssociateDTO associateDTO, AgendaDTO agendaDTO, VotingSavedDTO votingSavedDTO) {
-        return VoteEntity.builder()
+    private static final String YES = "sim";
+    private static final String NO = "nao";
+
+    public VotingEntity to(AssociateDTO associateDTO, AgendaDTO agendaDTO, VotingSavedDTO votingSavedDTO) {
+        return VotingEntity.builder()
                 .associate(associateMapper.to(associateDTO))
                 .agenda(agendaMapper.to(agendaDTO))
                 .vote(checkVote(votingSavedDTO))
@@ -31,20 +34,21 @@ public class VotingMapper {
 
     @SneakyThrows
     private static String checkVote(VotingSavedDTO votingSavedDTO) {
-        if (null == votingSavedDTO.getVote())
-            throw new PocAssembleiaException(PocSicredErrors.VOTE_INVALID);
-        if (votingSavedDTO.getVote()) {
-            return "sim";
-        } else {
-            return "nao";
+        if (YES.equals(votingSavedDTO.getVote())) {
+            return YES;
         }
+        if (NO.equals(votingSavedDTO.getVote())) {
+            return NO;
+        }
+        throw new PocAssembleiaException(PocSicredErrors.VOTE_INVALID);
     }
 
-    public VotingDTO from(List<VoteEntity> votesYes, List<VoteEntity> votesNo, AgendaEntity agenda) {
+    public VotingDTO from(List<VotingEntity> votesYes, List<VotingEntity> votesNo, AgendaEntity agenda) {
         return VotingDTO.builder()
                 .agendaDTO(agendaMapper.from(agenda))
                 .voteYes(String.valueOf(votesYes.size()))
                 .voteNo(String.valueOf(votesNo.size()))
                 .build();
     }
+
 }
