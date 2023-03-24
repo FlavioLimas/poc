bbcab525706f82746c92b4e7ae9ebcae022651b0
Flavio Lima <flaviolima.s@live.com>
2023-03-24 18:48:39 -0300

    WIP - add teste para end point save

--- src/test/java/com/sicred/poc/controller/AgendaControllerTest.java
+++ src/test/java/com/sicred/poc/controller/AgendaControllerTest.java
@@ -2,22 +2,27 @@ package com.sicred.poc.controller;
 
 import com.sicred.poc.external.dto.AgendaDTO;
 import com.sicred.poc.external.dto.AgendaSaveDTO;
+import com.sicred.poc.mapper.AgendaMapper;
+import com.sicred.poc.repository.AgendaRepository;
 import com.sicred.poc.service.impl.AgendaService;
 import com.sicred.poc.template.AgendaTemplate;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.net.URI;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class AgendaControllerTest {
@@ -51,7 +56,9 @@ class AgendaControllerTest {
     @Test
     @DisplayName("Should Pass When Save CREATED")
     void testShouldPassWhenSaveCREATED() {
-        AgendaSaveDTO agendaSaveDTO = AgendaTemplate.valid();
+        AgendaSaveDTO agendaSaveDTO = AgendaTemplate.validAgendaSaveDTO();
+        when(service.save(any())).thenReturn(ResponseEntity.created(URI.create("/agenda/save"))
+                        .body(AgendaTemplate.validAgendaDTO()));
         ResponseEntity<AgendaDTO> response = controller.save(agendaSaveDTO);
         assertEquals(HttpStatus.CREATED, response.getStatusCode(),
                 "Assertion fail, response status invalid");
--- src/test/java/com/sicred/poc/template/AgendaTemplate.java
+++ src/test/java/com/sicred/poc/template/AgendaTemplate.java
@@ -1,11 +1,20 @@
 package com.sicred.poc.template;
 
+import com.sicred.poc.external.dto.AgendaDTO;
 import com.sicred.poc.external.dto.AgendaSaveDTO;
 
 public class AgendaTemplate {
 
-    public static AgendaSaveDTO valid() {
+    public static AgendaSaveDTO validAgendaSaveDTO() {
         return AgendaSaveDTO.builder()
+                .title("Titulo da pauta teste")
+                .description("Descricao da pauta teste")
+                .build();
+    }
+
+    public static AgendaDTO validAgendaDTO() {
+        return AgendaDTO.builder()
+                .id(1L)
                 .title("Titulo da pauta teste")
                 .description("Descricao da pauta teste")
                 .build();
