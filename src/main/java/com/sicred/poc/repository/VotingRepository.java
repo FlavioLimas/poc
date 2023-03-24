0e6f96ee5f9c4ad892b83959601b6cec090b68ca
Flavio Lima <flaviolima.s@live.com>
2023-03-23 21:34:29 -0300

    WIP - defnicao de metodo para contagem dos votos

--- src/main/java/com/sicred/poc/repository/VotingRepository.java
+++ src/main/java/com/sicred/poc/repository/VotingRepository.java
@@ -5,7 +5,11 @@ import com.sicred.poc.model.id.VotingEntityId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VotingRepository extends JpaRepository<VotingEntity, VotingEntityId> {
+
+    Optional<VotingEntity> findByVote(String vote);
 
 }
